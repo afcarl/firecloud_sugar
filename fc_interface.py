bc4a22089dfe75dda9a7047f782ec4ce64fb0825
Mimoun Cadosch <mimoun@wm0d5-ffd.broadinstitute.org>
2017-10-26 17:02:01 -0400

    cohorts update without duplicate samples

--- fc_interface.py
+++ fc_interface.py
@@ -2,8 +2,14 @@ import os
 import pandas as pd
 from firecloud import api as firecloud_api
 import datetime
+import glob
 
+################################################
 ### Interfacing with FC entities
+################################################
+
+blacklist = ["DW039-Tumor-SM-DB2IF"]
+
 def delete_sample(namespace, workspace, sample_id):
 	"""Delete sample from workspace/namespace
 	Args: 
@@ -74,7 +80,9 @@ def upload_entities_from_tsv(namespace, workspace, entities_tsv_file):
     res = firecloud_api.upload_entities_tsv(namespace, workspace, entities_tsv=entities_tsv_file)
     return res
 
+################################################
 ### Get functions
+################################################
 def get_sample_by_id(sample_id):
     """Get sample given its sample_id
     Args:
@@ -83,7 +91,9 @@ def get_sample_by_id(sample_id):
     all_samples = fc_interface.get_samples_multiple_batches(paths_to_samples_info, google_bucket_id)
     return all_samples[all_samples['entity:sample_id'] == sample_id]
 
+################################################
 ### PoN Functions
+################################################
 def create_panel_of_normals(paths, N, name):
     """Create panel of normals sample set for Firecloud from multiple TSCA batches.
     Randomly select N samples from samples present in files listed in paths.
@@ -103,20 +113,86 @@ def create_panel_of_normals(paths, N, name):
     if N==-1: print ("Creating panel of %d normals" %normals.shape[0])
     else: print ("Creating panel of %d normals" %N)
     
+    # Compile data
     data = pd.concat([pd.DataFrame(index=normals.index, columns=['membership:sample_set_id'], data=name), \
                         normals], axis=1)
 
     return data
 
-def create_panel_of_normals_from_batch(batch_id, paths, N, name):
-    """Create panel of normals with samples from a given batch. Add N random samples from other batches.\
+def upload_cohort_all_tumors(paths_to_samples_info, google_bucket_id, name, namespace, workspace, blacklist=[]):
+    """Create and upload cohort of all tumor samples across all batches
+    Args: 
+        - Self-explanatory
+        - name: cohort name (usually Cum_Tumors_{LATEST_TSCA_ID}_all)
+        - paths_to_samples_info: .xlsx file containing paths to files containing sample_info
+    """
+    all_samples = get_samples_multiple_batches(paths_to_samples_info, google_bucket_id)
+    tumor_samples = all_samples[all_samples.sample_type == "Tumor"]
+
+    # Prepare column names
+    df = tumor_samples[['entity:sample_id']].rename(columns={'entity:sample_id': 'sample_id'})
+    df['membership:sample_set_id'] = name
+
+    # Re-arrange columns
+    cols = ['membership:sample_set_id', 'sample_id']
+    df = df[cols]
+
+    # Blacklist
+    df = df[ ~df['sample_id'].isin(blacklist) ]
+    df.to_csv('tumor_samples/fc_upload_%s.txt'%name, index=None, sep="\t")
+    res = upload_entities_from_tsv(namespace, workspace, 'tumor_samples/fc_upload_%s.txt'%name)
+    return res
+
+def upload_cohort_all_samples(paths_to_samples_info, google_bucket_id, name, namespace, workspace, blacklist=[]):
+    """Create and upload cohort all samples
+    Args: 
+        - Self-explanatory
+    """
+    all_samples = get_samples_multiple_batches(paths_to_samples_info, google_bucket_id)
+    df = all_samples[['entity:sample_id']].rename(columns={'entity:sample_id': 'sample_id'})
+    df['membership:sample_set_id'] = name
+
+    # Re-arrange columns
+    cols = ['membership:sample_set_id', 'sample_id']
+    df = df[cols]
+
+    # Blacklist
+    df = df[ ~df['sample_id'].isin(blacklist) ]
+    df.to_csv('all_samples/fc_upload_%s.txt'%name, index=None, sep="\t")
+    res = upload_entities_from_tsv(namespace, workspace, 'all_samples/fc_upload_%s.txt'%name)
+    return res
+
+def create_panel_of_normals_from_batch(batch_id, paths_to_samples_info, N=20):
+    """Create panel of normals with samples from a given batch. Add N random samples from other batches.
     Args:
         batch_id: (int) batch_id of batch in question
-        paths: (list) paths to file ending in {}.import_samples.txt
+        paths_to_samples_info: .xlsx file containing paths to files containing sample_info
         N: (int) number of samples in panel of normals
-        name: (string) name of Panel of Normals
+    Returns: 
+        pon: pon df
+        name: pon name
     """
-    ### FINISH
+
+    paths_to_samples = pd.read_excel(paths_to_samples_info, index_col=0)
+    path_to_samples_info  = paths_to_samples.loc[batch_id, 'path_to_samples_info']
+    all_samples = pd.read_table(path_to_samples_info)
+    normal_samples = all_samples[all_samples['sample_type'] == "Normal"]
+
+    # Iterate over batches except the one in question (to select N random normal samples from them)
+    dfs = []
+    for i, row in paths_to_samples.loc[~paths_to_samples.index.isin([batch_id])].iterrows():
+        df_tmp = pd.read_table(row['path_to_samples_info'])
+        dfs.append(df_tmp)
+    df = pd.concat(dfs, axis=0)
+
+    other_normal_samples = df[df['sample_type'] == "Normal"].sample(N)
+    
+    name = 'PoN_%s_plus_%s_random' %(batch_id, N)
+    pon = pd.concat([normal_samples, other_normal_samples], axis=0)
+    pon['membership:sample_set_id'] = name
+    pon = pon[['membership:sample_set_id', 'sample_id']]
+
+    return pon, name
 
 def upload_pon(pon_df, pon_name, namespace, workspace):
     """Upload PoN to FC
@@ -130,7 +206,20 @@ def upload_pon(pon_df, pon_name, namespace, workspace):
     res = upload_entities_from_tsv(namespace, workspace, 'PoNs/fc_upload_PoN_%s.txt'%pon_name)
     return res
 
+    res = fc_interface.upload_entities_from_tsv(namespace, workspace, cohorts_sample_set_metadata)
+
+def upload_cohorts(namespace, workspace, tsca_id):
+    """Upload cohort metadata to FC
+    Args: 
+        - self-explanatory
+    """
+    cohorts_sample_set_metadata = "cohort_files/fc_upload_sample_set_cohorts_%s.txt"%tsca_id
+    res = upload_entities_from_tsv(namespace, workspace, cohorts_sample_set_metadata)
+    return res
+
+################################################
 ### Samples Functions
+################################################
 def get_samples_multiple_batches(paths_to_samples_info, google_bucket_id, sublist=None):
     """Compile samples from multiple batches
     Args: Self-explanatory
@@ -152,7 +241,32 @@ def get_samples_multiple_batches(paths_to_samples_info, google_bucket_id, sublis
     all_samples = pd.concat(df_list, axis=0)
     return all_samples
 
+def merge_walkupseq_files(paths_to_batches_info):
+    """Merge all walkupseq files in the walkupseq directory
+    """
+    paths_to_batches_info = glob.glob('walkupseq_files/*sample_info*')
+
+    dfs = []
+    for f in paths_to_batches_info:
+        tmp = pd.read_table(f, encoding='latin1')
+        dfs.append(tmp)
+
+    df = pd.concat(dfs, axis=0)
+    df.to_csv('walkupseq_files/walkupseq_all_combined.txt', sep="\t", index=None)
+    return df
+
+def download_remote_samples(namespace, workspace):
+    """Download remote samples from Firecloud
+    Writes:
+        - remote_samples.txt: samples in FC
+    """
+    res = firecloud_api.get_entities_tsv(namespace, workspace, "sample")
+    with open('remote_samples.txt', 'w') as outfile:
+        outfile.write(res.text)
+    return
+################################################
 ### Pairs Functions
+################################################s
 def create_pairs_list(all_samples):
     """Creates DF with pairs for firecloud metadata export.
     Args:
@@ -277,8 +391,10 @@ def upload_pairsets(namespace, workspace, pairsets, pairset_type):
     res = upload_entities_from_tsv(namespace, workspace, filename)
     return res
 
+################################################
 ### Preparations for FC export
 # These functions create files used to export data to FC
+################################################
 def prepare_batch_samples_for_metadata_export(path, tsca_id, google_bucket_id):
     """Prepare the file to export samples metadata to firecloud
     Args:
@@ -311,6 +427,7 @@ def prepare_batch_samples_for_metadata_export(path, tsca_id, google_bucket_id):
                    %(path_in_bucket_full, row['external_id_validation'], row['bai_filename']), axis=1)
     # Add TSCA ID
     data['tsca_id'] = tsca_id
+
     data.to_csv('%s/fc_upload_samples_tsca_%s.txt' % (tsca_id, tsca_id), sep='\t', index=False)
     return data
 
@@ -348,17 +465,17 @@ def prepare_batch_sample_set_for_metadata_export(path, tsca_id):
     normals.to_csv( './%s/fc_upload_sample_set_tsca_%s_normals.txt' % (tsca_id, tsca_id), sep="\t", index=False )
     return (all_samples, tumors, normals)
 
-def prepare_patients_for_metadata_export(path, tsca_id):
+def prepare_patients_for_metadata_export(path_to_samples_info, tsca_id):
     """Create participant entities file for Firecloud. 
     Patients need to exist before you can upload their respective samples
     Args:
-        path_id: path to file ending in {}.export_samples.txt
+        path_id: path to file ending in {}.import_samples.txt
         tsca_id: tsca id
     Saves: 
         ./tsca_id/fc_upload_patients_tsca_{tsca_id}.csv:
             contains patient ids in tsca batch
     """    
-    raw = pd.read_table(path)
+    raw = pd.read_table(path_to_samples_info)
     print( "%d Participants in this batch" % raw['individual_id'].unique().shape[0] )
     # Data to upload
     data = pd.DataFrame(raw.individual_id.drop_duplicates()).rename(columns={'individual_id':'entity:participant_id'})
@@ -367,18 +484,90 @@ def prepare_patients_for_metadata_export(path, tsca_id):
     data.to_csv(filename, '\t', index=False)
     return data
 
-def prepare_all_data_for_metadata_export(tsca_id, path_to_batch_samples_info, path_to_all_samples_info, google_bucket_id):    
+def prepare_cohorts_for_metadata_export(paths_to_batches_info, google_bucket_id, blacklist, tsca_id, namespace, workspace):
+    """Creates sample sets corresponding to cohorts for Firecloud export.
+    Args:
+        - tsca_id: latest tsca_id
+        - paths_to_batches_info: file containing paths to all paths_to_batch_samples_info files (usually paths_to_batches_info.xlsx)
+    Writes:
+        - in cohort_files: fc_upload_sample_set_{cohort_name}.txt
+    Returns: 
+        - data_clean 
+    """
+    # Retrieve list of samples with corresponding cohort from bsp.broadinstitute.org
+    samples_with_cohort = pd.read_excel('cohort_files/bsp_latest_all_samples_%s.xls'%tsca_id)
+
+    # All samples, without cohort data
+    all_samples = get_samples_multiple_batches(paths_to_batches_info, google_bucket_id)
+
+    # Add cohort data to all samples
+    data = pd.merge(all_samples, samples_with_cohort[['Sample ID', 'Collection']], \
+                     left_on='bsp_sample_id_validation', \
+                     right_on='Sample ID', \
+                     how='inner') \
+                    .drop(['Sample ID'], axis=1)
+
+    # FC doesn't accept cohort names with non-alphanumeric characters, so use cohort codes instead
+    # Load dictionary of {long cohort name : short cohort code}
+    cohort_formatted_names = pd.read_table('cohort_files/cohort_names_dictionary.txt', \
+                                           header=None, names=['Collection', 'cohort_code'])
+
+    # Add cohort codes to data
+    data = pd.merge(data, cohort_formatted_names, on='Collection', how='outer')
+
+    # Prepare for FC export format
+    data = data.rename(columns={'cohort_code': 'membership:sample_set_id', 'entity:sample_id': 'sample_id'})
+    data_clean = data[['membership:sample_set_id', 'sample_id']]
+
+    # Remove blacklist
+    data_clean = data_clean[ ~data_clean['sample_id'].isin(blacklist)]
+
+    # Samples in new batch
+    samples_new_batch = pd.read_table("%s/fc_upload_sample_set_tsca_%s.txt"%(tsca_id, tsca_id))
+    
+    # Only upload new samples. As of 10/17, Firecloud doesn't filter duplicate entries
+    data_clean = data_clean[ data_clean['sample_id'].isin(samples_new_batch.sample_id.tolist()) ]
+
+    # Write FC import file 
+    data_clean.to_csv('cohort_files/fc_upload_sample_set_cohorts_%s.txt'%tsca_id, index=False, sep="\t")
+    
+    return data_clean
+
+def prepare_all_data_for_metadata_export(tsca_id, path_to_batch_samples_info, paths_to_batches_info, google_bucket_id):    
     """Prepare all batch metadata for uploading to Firecloud
     Args:
         - path_to_batch_samples_info: path to info on batch samples
-        - path_to_all_samples_info: path to info on all samples
     """
     prepare_patients_for_metadata_export(path_to_batch_samples_info, tsca_id)
     prepare_batch_sample_set_for_metadata_export(path_to_batch_samples_info, tsca_id)
     prepare_batch_samples_for_metadata_export(path_to_batch_samples_info, tsca_id, google_bucket_id)
+    prepare_cohorts_for_metadata_export(paths_to_batches_info, google_bucket_id)
     return
 
+################################################
+### Prepare metadata
+# These functions prepare the walkupseq and BSP metadata
+################################################
+def update_walkup_sheets(tsca_id):
+    """The walkupseq sheets contain experimental data (collaborator, tumor type, tissue site, etc.).
+    Compile all the sheets onto one walkupseq_all_combined_TSCAXX.txt
+    Args: 
+    - tsca_id: latest TSCA id added
+    Writes: 
+    - walkupseq_files: walkupseq_all_combined_TSCA{XX}.txt: combined data from all walkupseq files
+    """
+    files = glob.glob('/Users/mimoun/production_fc/metadata_exports/walkupseq_files/tsca*')
+    dfs = []
+    for f in files:
+        dfs.append( pd.read_table(f, comment='#', encoding='latin1'))
+    combined = pd.concat(dfs, axis=0)
+    fname = "/Users/mimoun/production_fc/metadata_exports/walkupseq_files/walkupseq_all_combined_%s.txt"%tsca_id
+    combined.to_csv(fname, sep="\t", index=None)
+    return combined
+
+################################################
 ### Export batch metadata to FC
+################################################
 def export_batch_metadata_to_fc(tsca_id, namespace, workspace):
     """Export metadata into Firecloud
     Args: 
@@ -390,6 +579,7 @@ def export_batch_metadata_to_fc(tsca_id, namespace, workspace):
     normals_sample_set_metadata = "%s/fc_upload_sample_set_tsca_%s_normals.txt" % (tsca_id, tsca_id)
     samples_metadata            = "%s/fc_upload_samples_tsca_%s.txt" % (tsca_id, tsca_id)
     pon_metadata                = "PoNs/fc_upload_PoN_sample_set_tsca_%s_PoN.txt" %(tsca_id)
+    # cohorts_sample_set_metadata = "cohort_files/fc_upload_sample_set_cohorts.txt"
 
     # Upload metadata
     r1 = upload_entities_from_tsv(namespace, workspace, patient_metadata)
@@ -397,22 +587,24 @@ def export_batch_metadata_to_fc(tsca_id, namespace, workspace):
     r3 = upload_entities_from_tsv(namespace, workspace, sample_set_metadata)
     r4 = upload_entities_from_tsv(namespace, workspace, tumors_sample_set_metadata)
     r5 = upload_entities_from_tsv(namespace, workspace, normals_sample_set_metadata)
-    return (r1, r2, r3, r4, r5)
+    # r6 = upload_entities_from_tsv(namespace, workspace, cohorts_sample_set_metadata)
+    return (r1, r2, r3, r4, r5) #, r6)
 
-def update_batch_metadata(tsca_id, path_to_batch_samples_info, path_to_all_samples_info, namespace, workspace, google_bucket_id):
+def update_batch_metadata(tsca_id, path_to_batch_samples_info, paths_to_batches_info, namespace, workspace, google_bucket_id):
     """Upload to Firecloud all the metadata necessary to run TSCA pipeline on new batch
     Args: 
         - tsca_id: tsca_id of the batch to run TSCA pipeline on
         - path_to_batch_samples_info: ends in *.export_samples.txt
-        - path_to_all_samples_info: .xlsx file with path_to_batch_samples_info for all batches
     """
     # Prepare all metadata for batch
-    prepare_all_data_for_metadata_export(tsca_id, path_to_batch_samples_info, path_to_all_samples_info, google_bucket_id)
+    prepare_all_data_for_metadata_export(tsca_id, path_to_batch_samples_info, paths_to_batches_info, google_bucket_id)
     # Upload to Firecloud
     export_batch_metadata_to_fc(tsca_id, namespace, workspace)
     return
 
+################################################
 ### Download method configs
+################################################
 def download_method_configs(namespace, workspace):
     """Download the latest method configs being used in firecloud, and write them to file
     """
